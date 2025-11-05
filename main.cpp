6b58184b897f86e58e80349df0f34b25cea74b52
250072-dot <250072@newuu.uz>
2025-11-05 12:59:24 +0500

    changed

--- main.cpp
+++ main.cpp
@@ -1,46 +1,50 @@
 #include <iostream>
 using namespace std;
 
-const int size = 4;
-double sumColumn(const double [][size],int rowsize,int columnIndex){
-int sum = 0;
-for (int i = 0; i < rowsize; i++){
-for (int j = 0; j < columnIndex; j++){cout <<"enter : "; cin >> column[i][j];}
-}
-for (int i = 0; i < size; i++){ for (int j=0; j < rowsize; j++){ sum += column[i][j];}
-if (columnIndex == size ) return sum;
+const int SIZE = 4;
+double sumColumn(const double m[][SIZE],int rowsize,int columnIndex) {
+    int sum = 0;
+    for (int i = 0; i < rowsize; i++){sum += m[i][columnIndex];}
+
+    return sum;
 }
 
-
-
+//problem 2ww
+double summajordiagonal( const double m[SIZE][SIZE]) {
+    double sum = 0;
+    for (int i = 0; i < SIZE; i++) {
+        sum += m[i][i];
+    }
+    return sum;
+}
 int main() {
 
- //problem 1
-
-cout << sumColumn({{1,2,3},{4,5,6},{7,8,9}});
-
-
-
-
-
-
-
-
-
-
-
-
-
+    /*       //problem 1
+           int rows;
+        cout << "Enter number of rows: ";
+           cin >> rows;
+           double arr[rows][SIZE];
+           for (int i = 0; i < rows; i++) {
+               for (int j = 0; j < SIZE; j++) {
+                   cout <<" enter: ";
+                   cin >> arr[i][j];
+               }
+           }
+           for (int i = 0; i < SIZE; i++) {
+               cout << sumColumn(arr,rows,i) << endl;
+           }
 
 
+       //problem 2
+           double m[SIZE][SIZE];
+       for (int i = 0; i < SIZE; i++) {
+           for (int j = 0; j < SIZE; j++) {
+               cin >> m[i][j];
+           }
+       }
+       cout << summajordiagonal(m) << endl;*/
+
+    //problem 3
 
 
-
-
-
-
-
-
-
-
-return 0;}
+    return 0;}
